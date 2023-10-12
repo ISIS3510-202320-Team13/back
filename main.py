1e27319c3519a03253a29c639bcb4a40fad9aaa4
Juan Diego Lugo Sánchez <60227071+JuanLugoS@users.noreply.github.com>
2023-10-12 18:07:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -38,13 +38,36 @@ def read_item(lat: float, lon: float):
     users_ref = db.collection("parkings")
     docs = users_ref.stream()
     
+    choice = ""
+    rate = -1
+    price = 0
     ret = {}
+
     for doc in docs:
         docdic = doc.to_dict()
+
         dist = haversine((lat, lon), (float(docdic["coordinates"].latitude), float(docdic["coordinates"].longitude)), unit=Unit.METERS)
+        t_price = docdic["price"]
+        t_rate = docdic["rating"]*t_price
+        
+
+        if (rate == -1) or (rate > (t_rate)):
+            rate = t_rate
+            price = t_price
+            choice = doc.id
+
         if dist <= 500:
-            docdic["distance"] = dist
+            docdic["distance"] = round(dist, 2)
+            docdic["choice"] = False
             ret[doc.id] = docdic
     
-    return ret
+    temp = ret.copy()
+    p_choosed = temp.pop(choice)
+    if price == p_choosed["price"]:
+        p_choosed["price_match"] = True
+        p_choosed["choice"] = True
+
+    temp["choice"] = p_choosed
+
+    return temp
     
