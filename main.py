f017098052a4a22b40081ad5447e9359a1d9f50c
Juan Diego Lugo Sánchez <60227071+JuanLugoS@users.noreply.github.com>
2023-10-12 20:17:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -76,34 +76,36 @@ def read_item(lat: float, lon: float):
     rate = -1
     price = 0
     ret = {}
+    temp = {}
 
-    for doc in docs:
-        docdic = doc.to_dict()
+    if len(docs) != 0:
+        for doc in docs:
+            docdic = doc.to_dict()
 
-        dist = haversine((lat, lon), (float(docdic["coordinates"].latitude), float(docdic["coordinates"].longitude)), unit=Unit.METERS)
-        t_price = docdic["price"]
-        t_rate = docdic["rating"]*t_price
+            dist = haversine((lat, lon), (float(docdic["coordinates"].latitude), float(docdic["coordinates"].longitude)), unit=Unit.METERS)
+            t_price = docdic["price"]
+            t_rate = docdic["rating"]*t_price
+            
+
+            if (rate == -1) or (rate > (t_rate)):
+                rate = t_rate
+                price = t_price
+                choice = doc.id
+
+            if dist <= 500:
+                docdic["distance"] = round(dist, 2)
+                docdic["choice"] = False
+                ret[doc.id] = docdic
         
+        temp = ret.copy()
+        p_choosed = temp.pop(choice)
+        if price == p_choosed["price"]:
+            p_choosed["price_match"] = True
+        else:
+            p_choosed["price_match"] = False
+        p_choosed["choice"] = True
 
-        if (rate == -1) or (rate > (t_rate)):
-            rate = t_rate
-            price = t_price
-            choice = doc.id
-
-        if dist <= 500:
-            docdic["distance"] = round(dist, 2)
-            docdic["choice"] = False
-            ret[doc.id] = docdic
-    
-    temp = ret.copy()
-    p_choosed = temp.pop(choice)
-    if price == p_choosed["price"]:
-        p_choosed["price_match"] = True
-    else:
-        p_choosed["price_match"] = False
-    p_choosed["choice"] = True
-
-    temp["choice"] = p_choosed
+        temp["choice"] = p_choosed
 
     return temp
     
