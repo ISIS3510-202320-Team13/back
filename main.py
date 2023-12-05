a5391d3013240ee3e31303928b3ce2a31549e141
Juan Diego Lugo Sánchez <60227071+JuanLugoS@users.noreply.github.com>
2023-12-04 23:32:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -232,7 +232,7 @@ async def get_parkings_bylatlon(lat: float, lon: float, api_key: str = Security(
 
 # ------------------------------------ Utils ------------------------------------
 @app.get("/confirmation/{uid}/", tags=["Utils"])
-async def send_confirmation_email(uid: str, api_key: str = Security(get_api_key)):
+async def send_confirmation_email(uid: str, api_key: str = Security(get_api_key), status_code=200):
     """
     This function will allow you get a complete collection as is stored in the database
 
@@ -247,10 +247,10 @@ async def send_confirmation_email(uid: str, api_key: str = Security(get_api_key)
 
     """
     try:
-        response = controller.send_email_comfirmation(uid)
+        controller.send_email_comfirmation(uid)
     except Exception as e:
         raise HTTPException(status_code=404, detail=f"Looks like something went wrong with'/raw/collection/{uid}' --- {e}")
-    return response
+    return {'response':'ok'}
 
 @app.get("/raw/collection/{collection}", tags=["Utils"])
 async def get_raw_collection(collection: str, api_key: str = Security(get_api_key)):
