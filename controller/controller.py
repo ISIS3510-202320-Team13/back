fd879610eb4272ea676bf6bc9fb1638748667635
Juan Diego Lugo Sánchez <60227071+JuanLugoS@users.noreply.github.com>
2023-12-04 19:47:45 -0500

    Update controller.py

--- controller/controller.py
+++ controller/controller.py
@@ -22,7 +22,8 @@ def create_reservation(reservation_p:dict):
 
 def update_reservation(uid:str, reservation_p:dict):
     print(reservation_p)
-    for key in reservation_p:
+    copy = reservation_p.copy()
+    for key in copy:
         if reservation_p[key] == None:
             reservation_p.pop(key)
     reservation_p['uid'] = uid
